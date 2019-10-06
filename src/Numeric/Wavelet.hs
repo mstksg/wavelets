668494804688aa8e18db4e79f7c52fee5a1a2be8
Justin Le <justin@jle.im>
2019-10-06 00:15:45 -0700

    folders and bolders

--- src/Numeric/Wavelet.hs
+++ src/Numeric/Wavelet.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE KindSignatures                           #-}
 {-# LANGUAGE LambdaCase                               #-}
 {-# LANGUAGE NoStarIsType                             #-}
+{-# LANGUAGE RankNTypes                               #-}
 {-# LANGUAGE ScopedTypeVariables                      #-}
 {-# LANGUAGE StandaloneDeriving                       #-}
 {-# LANGUAGE TypeApplications                         #-}
@@ -15,20 +16,19 @@
 
 module Numeric.Wavelet where
 
--- module Numeric.Wavelet (
---   ) where
-
-
+import           Control.Monad
 import           Data.Finite
 import           Data.Kind
 import           Data.Proxy
-import           Data.Vector.Generic.Sized (Vector)
+import           Data.Vector.Generic.Sized          (Vector)
 import           GHC.TypeLits.Compare
 import           GHC.TypeNats
-import qualified Data.Vector               as UV
-import qualified Data.Vector.Generic       as UVG
-import qualified Data.Vector.Generic.Sized as VG
-import qualified Data.Vector.Sized         as V
+import qualified Data.Finite.Internal               as FI
+import qualified Data.Vector                        as UV
+import qualified Data.Vector.Generic                as UVG
+import qualified Data.Vector.Generic.Sized          as VG
+import qualified Data.Vector.Generic.Sized.Internal as VGI
+import qualified Data.Vector.Sized                  as V
 
 
 data WaveDec :: (Type -> Type) -> Nat -> Type -> Type where
@@ -46,6 +46,11 @@ _wdDetail = \case
     WDZ d _ -> VG.singleton d
     WDS d _ -> d
 
+wdApprox :: WaveDec v n a -> a
+wdApprox = \case
+    WDZ _ a -> a
+    WDS _ a -> wdApprox a
+
 testDec :: WaveDec UV.Vector 4 Double
 testDec = WDS (0 :: V.Vector 16 Double)
         . WDS (0 :: V.Vector  8 Double)
@@ -57,6 +62,27 @@ flattenWD :: UVG.Vector v a => WaveDec v n a -> Vector v (2 ^ (n + 1)) a
 flattenWD = \case
     WDZ d  a  -> VG.fromTuple (a, d)
     WDS ds as -> flattenWD as VG.++ ds
+
+itraverseDec
+    :: (UVG.Vector v a, UVG.Vector v b, KnownNat n, Applicative f)
+    => (a -> f b)
+    -> (forall q. (KnownNat q, q <= (n + 1)) => Finite (2 ^ q) -> a -> f b)
+    -> WaveDec v n a
+    -> f (WaveDec v n b)
+itraverseDec f g = \case
+    WDZ d  a  -> flip WDZ <$> f a <*> g @1 0 d
+    WDS ds as -> flip WDS <$> itraverseDec f g as
+                          <*> itraverseVector g ds
+
+ifoldDec
+    :: (UVG.Vector v a, KnownNat n, Monoid m)
+    => (a -> m)
+    -> (forall q. (KnownNat q, q <= (n + 1)) => Finite (2 ^ q) -> a -> m)
+    -> WaveDec v n a
+    -> m
+ifoldDec f g = \case
+    WDZ d  a  -> f a <> g @1 0 d
+    WDS ds as -> ifoldDec f g as <> ifoldMapVector g ds
 
 haar
     :: forall v n a. (UVG.Vector v a, KnownNat n, Fractional a)
@@ -101,4 +127,18 @@ unhaarPass app det = VG.generate $ \i ->
           | j == 0    = (+)
           | otherwise = (-)
     in  combiner (app `VG.index` k) (det `VG.index` k)
+
+itraverseVector
+    :: (UVG.Vector v a, UVG.Vector v b, Applicative f)
+    => (Finite n -> a -> f b)
+    -> Vector v n a
+    -> f (Vector v n b)
+itraverseVector f (VGI.Vector xs) = VGI.Vector . UVG.fromList <$> zipWithM f (FI.Finite <$> [0..]) (UVG.toList xs)
+
+ifoldMapVector
+    :: (UVG.Vector v a, Monoid m)
+    => (Finite n -> a -> m)
+    -> Vector v n a
+    -> m
+ifoldMapVector f (VGI.Vector xs) = foldMap (uncurry f) $ zip (FI.Finite <$> [0..]) (UVG.toList xs)
 
