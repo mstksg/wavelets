adc917ae293d63dab658961409202387eaa15b5c
Justin Le <justin@jle.im>
2019-10-05 23:41:45 -0700

    haar and unHaar

--- src/Numeric/Wavelet.hs
+++ src/Numeric/Wavelet.hs
@@ -4,8 +4,11 @@
 {-# LANGUAGE LambdaCase                               #-}
 {-# LANGUAGE NoStarIsType                             #-}
 {-# LANGUAGE ScopedTypeVariables                      #-}
+{-# LANGUAGE StandaloneDeriving                       #-}
+{-# LANGUAGE TypeApplications                         #-}
 {-# LANGUAGE TypeFamilyDependencies                   #-}
 {-# LANGUAGE TypeInType                               #-}
+{-# LANGUAGE TypeOperators                            #-}
 {-# LANGUAGE UndecidableInstances                     #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise       #-}
@@ -18,7 +21,9 @@ module Numeric.Wavelet where
 
 import           Data.Finite
 import           Data.Kind
+import           Data.Proxy
 import           Data.Vector.Generic.Sized (Vector)
+import           GHC.TypeLits.Compare
 import           GHC.TypeNats
 import qualified Data.Vector               as UV
 import qualified Data.Vector.Generic       as UVG
@@ -26,46 +31,51 @@ import qualified Data.Vector.Generic.Sized as VG
 import qualified Data.Vector.Sized         as V
 
 
-data N = Z | S N
+data WaveDec :: (Type -> Type) -> Nat -> Type -> Type where
+    WDZ :: { _wdDetailZ :: !a
+           , _wdApprox  :: !a
+           } -> WaveDec v 0 a
+    WDS :: { _wdDetailS :: !(Vector  v (2 ^ (n + 1)) a)
+           , _wdNext    :: !(WaveDec v n             a)
+           } -> WaveDec v (n + 1) a
 
-data SN :: N -> Type where
-    SZ :: SN 'Z
-    SS :: SN n -> SN ('S n)
+deriving instance (Show (v a), Show a) => Show (WaveDec v n a)
 
-type family NNat n where
-    NNat 'Z     = 0
-    NNat ('S n) = 1 + NNat n
-
-data WaveDec :: N -> (Type -> Type) -> Nat -> Type -> Type where
-    WDZ :: { _wdDetailZ :: Vector v (2 ^ n) a
-           , _wdApprox  :: Vector v (2 ^ n) a
-           } -> WaveDec 'Z v n a
-    WDS :: { _wdDetailS :: Vector    v (2 ^ n) a
-           , _wdNext    :: WaveDec q v (n - 1) a
-           } -> WaveDec ('S q) v n a
-
-deriving instance Show (v a) => Show (WaveDec q v n a)
-
-_wdDetail :: WaveDec q v n a -> Vector v (2 ^ n) a
+_wdDetail :: UVG.Vector v a => WaveDec v n a -> Vector v (2 ^ n) a
 _wdDetail = \case
-    WDZ d _ -> d
+    WDZ d _ -> VG.singleton d
     WDS d _ -> d
 
-testDec :: WaveDec ('S ('S 'Z)) UV.Vector 4 Double
+testDec :: WaveDec UV.Vector 4 Double
 testDec = WDS (0 :: V.Vector 16 Double)
-        . WDS (0 :: V.Vector 8 Double )
-        $ WDZ (0 :: V.Vector 4 Double ) (0 :: V.Vector 4 Double)
+        . WDS (0 :: V.Vector  8 Double)
+        . WDS (0 :: V.Vector  4 Double)
+        . WDS (0 :: V.Vector  2 Double)
+        $ WDZ 0 0
+
+flattenWD :: UVG.Vector v a => WaveDec v n a -> Vector v (2 ^ (n + 1)) a
+flattenWD = \case
+    WDZ d  a  -> VG.fromTuple (a, d)
+    WDS ds as -> flattenWD as VG.++ ds
 
 haar
-    :: (UVG.Vector v a, KnownNat n, 1 <= n, Fractional a)
-    => SN q
-    -> Vector v (2 ^ n) a
-    -> WaveDec q v (n - 1) a
-haar = \case
-    SZ   -> uncurry (flip WDZ) . haarPass
-    SS r -> \xs ->
-      let (a, d) = haarPass xs
-      in  WDS d $ haar r a
+    :: forall v n a. (UVG.Vector v a, KnownNat n, Fractional a)
+    => Vector  v (2 ^ (n + 1)) a
+    -> WaveDec v n a
+haar xs = case pNat (Proxy @n) of
+    PZ -> let x = xs `VG.index` 0
+              y = xs `VG.index` 1
+          in  WDZ ((x - y) / 2) ((x + y) / 2)
+    PS -> let (a, d) = haarPass xs
+          in  WDS d (haar a)
+
+unHaar
+    :: (UVG.Vector v a, KnownNat n, Num a)
+    => WaveDec v n a
+    -> Vector v (2 ^ (n + 1)) a
+unHaar = \case
+    WDZ d  a  -> VG.fromTuple (a + d, a - d)
+    WDS ds as -> unhaarPass (unHaar as) ds
 
 haarPass
     :: (UVG.Vector v a, KnownNat n, Fractional a)
@@ -80,51 +90,15 @@ haarPass xs = (app, det)
                               - xs `VG.index` combineProduct (1, i)
                               ) / 2
 
--- waveDec
---     :: SN q
---     -> Vector v (2 ^ n) a
---     -> WaveDec q v (n - 1) a
--- waveDec = \case
---     SZ -> 
-
--- convolve
---     :: forall v n m a. (KnownNat n, KnownNat m, UVG.Vector v a, Num a, 1 <= n + m)
---     => Vector v n a
---     -> Vector v m a
---     -> Vector v (n + m - 1) a
--- convolve x y = VG.generate $ \i -> sum $ map (go i) finites
---   where
---     go  :: Finite (n + m - 1) -> Finite n -> a
---     go n k
---       | Right j <- sub n k
---       , Just l  <- strengthenN j
---       = x `VG.index` k * y `VG.index` l
---       | otherwise = 0
-
--- convolve'
---     :: forall v n m a. (KnownNat n, KnownNat m, UVG.Vector v a, Num a, 1 <= n + m)
---     => Vector v n a
---     -> Vector v m a
---     -> Vector v (n + m - 1) a
--- convolve' x y = VG.generate $ \i -> sum $ _
---   where
---     go  :: Finite n -> Finite m -> a
---     go j k = x `VG.index` j * y `VG.index` k
-
-
-    -- (x `VG.index` k) * (y `VG.index` j)
-    --   where
-    --     j = n - k
-    -- (n - k))
-
--- newtype DWT v n a = DWT { getDWT :: Vector v (2^(n - 1)) (Vector v n a) }
-
--- data WaveletType = WTDaubechies
---                  | WTHaar
---                  | WTBSpline
-
--- wave :: (UVG.Vector v a, UVG.Vector v (Vector v n a))
---     => Vector v (2^n) a
---     -> DWT v n a
--- dwt = undefined
+unhaarPass
+    :: (UVG.Vector v a, KnownNat n, Num a)
+    => Vector v (2 ^ n) a
+    -> Vector v (2 ^ n) a
+    -> Vector v (2 ^ (n + 1)) a
+unhaarPass app det = VG.generate $ \i ->
+    let (j, k) = separateProduct @2 i
+        combiner
+          | j == 0    = (+)
+          | otherwise = (-)
+    in  combiner (app `VG.index` k) (det `VG.index` k)
 
