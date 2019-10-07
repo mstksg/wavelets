9d96a4ee56a52380ddb09dcb2f45d1d1535d7eb0
Justin Le <justin@jle.im>
2019-10-07 12:38:06 -0700

    fast convolution via fft

--- src/Numeric/Wavelet/Continuous.hs
+++ src/Numeric/Wavelet/Continuous.hs
@@ -22,11 +22,16 @@ module Numeric.Wavelet.Continuous (
   , cwd
   ) where
 
+import           Data.Complex
 import           Data.Finite
 import           Data.Maybe
 import           Data.Proxy
+import           Data.Type.Equality
 import           Data.Vector.Generic.Sized (Vector)
+import           GHC.TypeLits.Compare
 import           GHC.TypeNats
+import           Math.FFT.Base
+import           Numeric.Wavelet.Internal
 import qualified Data.Vector.Generic       as UVG
 import qualified Data.Vector.Generic.Sized as VG
 import qualified Data.Vector.Sized         as V
@@ -37,31 +42,41 @@ newtype CWD v n m a = CWD { cwdLines :: V.Vector m (CWDLine v n a) }
 
 data CWDLine v n a = CWDLine
     { cwdlData  :: Vector v n a
-    , cwdlScale :: a                  -- ^ Scale factor, in number of ticks
+    , cwdlScale :: Finite (n `Div` 2 + 1) -- ^ Scale factor, in number of ticks
     , cwdlCoI   :: Finite (n `Div` 2 + 1)
     }
   deriving Show
 
 -- | Morelet continuous wavelet decomposition.
-cwd :: forall v n m a. (UVG.Vector v a, KnownNat n, KnownNat m, RealFloat a, 1 <= n)
+cwd :: forall v n m a.
+     ( UVG.Vector v a
+     , UVG.Vector v (Complex a)
+     , KnownNat n
+     , KnownNat m
+     , FFTWReal a
+     , 1 <= n
+     )
     => Finite (n `Div` 2 + 1)         -- ^ minimum scale (period)
     -> Finite (n `Div` 2 + 1)         -- ^ maximum scale (period)
     -> Vector v n a
     -> CWD v n m a
 cwd minS maxS xs = CWD . VG.generate $ \i ->
       let s   = scaleOf i
-          coi = fromMaybe maxBound . packFinite . round $ sqrt 2 * s
-      in  case someNatVal (round (scaleOf i)) of
+      in  case someNatVal (round s) of
             SomeNat p@(Proxy :: Proxy q) ->
-              let ms :: Vector v (8 * q) a
-                  ms = morletScaled p
-                  ys :: Vector v (n + (8 * q) - 1) a
-                  ys = convolve xs ms
-              in  CWDLine (VG.slice (Proxy @(4 * q)) ys) s coi
+              case isLE (Proxy @1) (Proxy @(8 * q)) of
+                Just Refl ->
+                  let ms :: Vector v (8 * q) a
+                      ms = morletScaled p
+                      ys :: Vector v (n + (8 * q) - 1) a
+                      ys = convolve xs ms
+                      coi = fromMaybe maxBound . packFinite . round $ sqrt 2 * s
+                  in  CWDLine (VG.slice (Proxy @(4 * q)) ys) (round s) coi
+                Nothing -> undefined
   where
     n = natVal (Proxy @n)
     m = natVal (Proxy @m)
-    maxScale = fromIntegral maxS `min` (fromIntegral n / (2 * sqrt 2))
+    maxScale = fromIntegral maxS `min` (fromIntegral n / (3 * sqrt 2))
     minScale = fromIntegral minS `max` 1
     scaleStep = (log maxScale - log minScale) / (fromIntegral m - 1)
     scaleOf :: Finite m -> a
@@ -89,15 +104,31 @@ morletScaled _ = morlet
 -- morseF dω p γ = VG.generate $ \((* dω) . fromIntegral->ω) -> ω ** (p*p/γ) * exp (- (ω ** γ))
 
 convolve
-    :: forall v n m a. (KnownNat n, KnownNat m, UVG.Vector v a, Num a, 1 <= n + m)
+    :: forall v n m a.
+     ( UVG.Vector v a
+     , UVG.Vector v (Complex a)
+     , KnownNat n, 1 <= n
+     , KnownNat m, 1 <= m
+     , FFTWReal a
+     )
     => Vector v n a
     -> Vector v m a
     -> Vector v (n + m - 1) a
-convolve x y = VG.generate $ \i -> sum $ map (go i) finites
+convolve x y = VG.map realPart . ifft $ fft x' * fft y'
   where
-    go  :: Finite (n + m - 1) -> Finite n -> a
-    go n k
-      | Right j <- sub n k
-      , Just l  <- strengthenN j
-      = x `VG.index` k * y `VG.index` l
-      | otherwise = 0
+    x' = ((:+ 0) `VG.map` x) VG.++ 0
+    y' = ((:+ 0) `VG.map` y) VG.++ 0
+
+-- convolve
+--     :: forall v n m a. (KnownNat n, KnownNat m, UVG.Vector v a, Num a, 1 <= n + m)
+--     => Vector v n a
+--     -> Vector v m a
+--     -> Vector v (n + m - 1) a
+-- convolve x y = VG.generate $ \i -> sum $ map (go i) finites
+  -- where
+    -- go  :: Finite (n + m - 1) -> Finite n -> a
+    -- go n k
+    --   | Right j <- sub n k
+    --   , Just l  <- strengthenN j
+    --   = x `VG.index` k * y `VG.index` l
+    --   | otherwise = 0
